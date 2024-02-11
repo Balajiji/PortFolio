24a64359ba94e0ead9b8f128c8d5555ae4becc3f
Balaji <98873242+MoorthieBalaji@users.noreply.github.com>
2024-02-11 21:37:21 +0530

    Update NavBar.js

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -5,6 +5,7 @@ import Navbar from 'react-bootstrap/Navbar';
 import {Link} from 'react-router-dom'
 import '../Components/NavBar.css'
 export const NavBar = () => {
+
   return (
       <>
       <div className='bb'>
@@ -14,13 +15,13 @@ export const NavBar = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav"/>
         <Navbar.Collapse id="basic-navbar-nav ml-auto">
             <div className="justify-content-end flex-grow-1 pe-auto">
-          <Nav className="ml-auto justify-content-end">
-           <Nav.Link as={Link} to='/' style={{color:'	#ffcc00'}}>Home</Nav.Link>
-            <Nav.Link as={Link} to='/About' style={{color:'	#ffcc00'}}>About</Nav.Link>
-           <Nav.Link as={Link} to='/Skills' style={{color:'	#ffcc00'}}>Skills</Nav.Link>
-          <Nav.Link as={Link} to='/Resume' style={{color:'	#ffcc00'}}>Resume</Nav.Link> 
-           <Nav.Link as={Link} to='/Projects' style={{color:'	#ffcc00'}}>Projects</Nav.Link>
-           <Nav.Link as={Link} to='/Contact' style={{color:'	#ffcc00'}}>Contact Us</Nav.Link>
+          <Nav className="ml-auto justify-content-end " style={{marginTop:'30px'}}>
+           <Nav.Link className='dd' as={Link} to='/' style={{color:'	#ffcc00'}}>Home</Nav.Link>
+            <Nav.Link  className='dd' as={Link} to='/About' style={{color:'	#ffcc00'}}>About</Nav.Link>
+           <Nav.Link   className='dd'as={Link} to='/Skills' style={{color:'	#ffcc00'}}>Skills</Nav.Link>
+          <Nav.Link  className='dd' as={Link} to='/Resume' style={{color:'	#ffcc00'}}>Resume</Nav.Link> 
+           <Nav.Link  className='dd' as={Link} to='/Projects' style={{color:'	#ffcc00'}}>Projects</Nav.Link>
+           <Nav.Link  className='dd'  as={Link} to='/Contact' style={{color:'	#ffcc00'}}>Contact Us</Nav.Link>
         
           </Nav>
           </div>
