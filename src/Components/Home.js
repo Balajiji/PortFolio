62f928a2766c96279af535d8bc67ccadfb340c6c
Balaji <98873242+MoorthieBalaji@users.noreply.github.com>
2024-02-11 21:35:25 +0530

    Update Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,20 +3,33 @@ import { NavBar} from './NavBar'
 import {Button} from 'react-bootstrap'
 import '../Components/Home.css';
 import { FaInstagram ,FaLinkedin,FaFacebook, FaTwitter} from 'react-icons/fa'
+import { Link } from 'react-router-dom';
 export const Home = () => {
+  const handleDownload=()=> {const resumeFilePath = './Resume.pdf'; 
+  const link = document.createElement('a');
+
+  link.href = resumeFilePath;
+  link.download =  './Resume.pdf';
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+};
   return (
    <>
    <div className='bb' style={{height:'100vh'}}>
     <NavBar/>
     <div  className='bo text-warning' style={{marginTop:'150px'}}>
-        <h1 style={{fontSize:'4rem'}}>Hello, I'm BALAJI I.</h1>
-        <h2 style={{fontSize:'4rem',marginLeft:'55px'}}>Full Stack Developer</h2>
+      <div >
+        <h1  className='typewriter' style={{fontSize:'4rem'}}>Hello, I'm BALAJI I.<br/><span style={{transitiondelay:'4s'}}>Full Stack Developer</span></h1>
+        {/* <h2  className='typewriter' style={{fontSize:'4rem',marginLeft:'55px'}}>Full Stack Developer</h2> */}</div>
+        <div className='abt'>
         <div style={{marginTop:'30px'}}>
           <Button variant="outline-warning">Hire Me</Button>
-          <Button variant="outline-warning" style={{marginLeft:'8px'}}>Download Resume</Button>
+          <Button variant="outline-warning" style={{marginLeft:'8px'}} onClick={handleDownload}>Download Resume</Button>
         </div>
-        <div style={{marginTop:'20px'}}> <FaInstagram size={40} color="#ffcc00" />       <FaLinkedin size={40} color="#ffcc00" />  <FaTwitter size={40} color="#ffcc00" />  <FaFacebook size={40} color="#ffcc00" /> 
+        <div style={{marginTop:'20px'}}> <Link to={'https://www.instagram.com/mr__green82/?hl=en'} style={{textDecoration:'none'}}><FaInstagram size={40} color="#ffcc00" />  </Link>   <Link to={'https://www.linkedin.com/in/balaji-i-27496a214/'}><FaLinkedin size={40} color="#ffcc00" /></Link>  <FaTwitter size={40} color="#ffcc00" />  <Link to={'https://www.facebook.com/theribalaji.theribalaji.10'}><FaFacebook size={40} color="#ffcc00" /></Link> 
 </div> 
+</div>
     </div>
    </div>
    </>
